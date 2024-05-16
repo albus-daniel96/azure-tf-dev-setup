34cc3932eaa8095d122d0d390fd861c324407ed7
albus-daniel96 <63167677+albus-daniel96@users.noreply.github.com>
2024-05-16 18:24:48 +0530

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,12 @@
+variable "nsg_name" {
+  type = string
+  default = "west_wurope_subnet_nsg"
+}
+variable "network_name" {
+  type = string
+  default = "west_wurope_network"
+}
+variable address_space {} 
+variable dns_servers {} 
+variable "subnet_name_address" {}
+variable "network_tags" {}
